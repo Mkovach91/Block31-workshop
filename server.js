7e414410defdb2b37207f924e9152c1644f7fe55
matt k <matt.kovach91@gmail.com>
2024-11-12 14:07:55 -0600

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 // TODO: this file :)
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+const employees = require("./employees")
+
+app.get("/", (req, res) => {
+  res.send("Hello Employees!");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const { id } = req.params;  
+  const employee = employees.find(emp => emp.id === +id);
+
+  if (employee) {
+    res.json(employee);
+  } else {
+    res.status(404).send("employee not found");
+  }
+});
+
+app.get("/employees/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  const randomEmployee = employees.find(randomIndex);
+  res.json(randomEmployee);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
